a5af9efe46a55d9168148096d8efefdaa4ba29c2
PyYourDaYe <li5418@126.com>
2018-12-17 03:18:35 +0800

    存入mongo库

--- JXWY/解析雷达/test_ok_asc.py
+++ JXWY/解析雷达/test_ok_asc.py
@@ -4,12 +4,21 @@
 import re
 import time
 
+from pymongo import MongoClient
+
+client = MongoClient(host='127.0.0.1', port=27017, userName='lizehua', password='lizehua')
+print(client.address)
+print(client.database_names())
+db = client.hehe
+print(db.collection_names())
+col = db['jxwy']
+# col.remove({})
+
 filename = r'test.trs'
 filename2 = r'C:\Users\zhd\Desktop\liebiao_2.trs'
 begin = time.time()
 final_count = 0
 erro_count = 0
-dicContent = {}  # 以字典格式存储每一条数据
 mix_list = []    # 汇总到列表
 
 def read_file(filename):
@@ -17,24 +26,24 @@ def read_file(filename):
         allStr = ""    # 将所有内容拼接到一起
         for line in file:
             allStr += line.strip()
-            if 'IR_SID'in line:
+            if 'IR_PUBTYPE'in line:
                 yield allStr
                 allStr = ""
 
 # for循环解析迭代对象的字符串
 # i = 0
-for everyStr in read_file(filename2):
+for everyStr in read_file(filename):
     final_count += 1
     print(everyStr)
+    dicContent = {}  # 以字典格式存储每一条数据
     # i += 1
     # if i >= 10:
     #     break
     '=====================================华丽的分割线============================================'
     try:
-        dicContent["SID"] = re.search("<IR_SID>=(.*)", everyStr).group(1)
+        dicContent["SID"] = re.search("<IR_SID>=(.*?)<IR_HKEY>", everyStr).group(1)
         dicContent["urlname"] = re.search("<IR_URLNAME>=(.*?)<IR_.+?>=", everyStr).group(1)
-        dicContent["loadtime"] = re.search("<IR_LASTTIME>=(.*?)<IR_.+?>=", everyStr).group(1).replace(".", "-")
-        dicContent["updatatime"] = dicContent["loadtime"]
+        dicContent["updatatime"] = re.search("<IR_LASTTIME>=(.*?)<IR_.+?>=", everyStr).group(1).replace(".", "-")
         dicContent["urltime"] = re.search("<IR_URLTIME>=(.*?)<IR_.+?>=", everyStr).group(1).replace(".", "-")
         dicContent["urltitle"] = re.search("<IR_URLTITLE>=([\s\S]*?)<IR_.+?>=", everyStr).group(1)
         dicContent["category"] = re.search("<IR_CHANNEL>=(.*?)<IR_.+?>=", everyStr).group(1)
@@ -91,9 +100,16 @@ for everyStr in read_file(filename2):
     print(dicContent)
     mix_list.append(dicContent)
 
+    # col.update({"urlname":dicContent["urlname"]},dicContent, upsert=True)  # 条件存在则修改，无则新建插入
+    col.update({'urlname':dicContent["urlname"]},{'$setOnInsert':dicContent},upsert=True)  # 条件存在则pass，无则新建插入
     print('-------------------------------------------------------------')
     '=====================================华丽的分割线============================================'
 
 print(time.time()-begin)
 print('处理数据的数量：' + str(final_count))
 print(len(mix_list))
+print('=====================================================')
+result = col.find()
+for data in result:
+    print(data)
+print(col.count())
--- monggo_package/monggo_ceshi.py
+++ monggo_package/monggo_ceshi.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 
-client = MongoClient(host='127.0.0.1',port=27017)
+client = MongoClient(host='127.0.0.1',port=27017,userName='lizehua',password='lizehua')
 print(client.address)
 print(client.database_names())
 db = client.hehe
 print(db.collection_names())
-col = db['stu']
+col = db['jxwy']
+col.update({'fuck':'fufufufufu'},{'age':'haha'},upsert=True)
 result = col.find()
 for data in result:
     print(data)
